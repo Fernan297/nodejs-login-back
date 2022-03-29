c7a422e19ce73f9b6416ade74db3de51cd4f4b5b
fer <fer@Fer.huawei.net>
2022-03-29 11:08:18 -0600

    Add out and modity index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Ejecutando');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+// Forma de capturar el body en las peticiones
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+
+// Conexion a la Base de datos
+
+// import routes
+const authRoutes = require('./routes/auth.js');
+
+// route del middleware
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mesaje: 'WORS!'
+    });
+});
+
+app.get('/api/user', authRoutes);
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Listening in port: ${PORT}`);
+});
